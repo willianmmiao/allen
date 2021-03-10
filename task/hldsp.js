2a776dc2c08a959a05716cb595ad1a10afb5a733
allenqq1209 <77651462+allenqq1209@users.noreply.github.com>
2021-03-10 17:56:47 +0800

    Update hldsp.js

--- task/hldsp.js
+++ task/hldsp.js
@@ -158,7 +158,7 @@ function hldspqd(timeout = 0) {
 
 hlsign = hldspurl.match(/sign=(\w+)/)[1]
 hluid = hldspurl.match(/uid=(\w.+)/)[1]
-
+  return new Promise((resolve) => {	
 let url = {
         url : 'https://vip.75787.com/app/index.php?i=3&t=0&v=1.1.47&from=wxapp&c=entry&a=wxapp&do=Doqiandao&m=luobo_video&sign='+hlsign+'&uid='+hluid,
         headers : JSON.parse(hldsphd),
